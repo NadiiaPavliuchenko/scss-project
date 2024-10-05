7757d3440db99060151b4063263fa65172ba5f70
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-10-05 20:20:07 +0300

    start styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 import "../scss/style.scss";
+import * as bodyScrollLock from "body-scroll-lock";
+
+const refs = {
+  openMenu: document.querySelector(".burgerBtn"),
+  backdrop: document.querySelector(".backdrop"),
+};
+
+function toggleMenu() {
+  refs.backdrop.classList.toggle("is-open");
+  const isMenuOpen = refs.backdrop.classList.contains("is-open");
+
+  const scrollLockMethod = isMenuOpen
+    ? "disableBodyScroll"
+    : "enableBodyScroll";
+  bodyScrollLock[scrollLockMethod](document.body);
+}
+
+refs.openMenu.addEventListener("click", toggleMenu);
+
+window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  if (!e.matches) return;
+  refs.backdrop.classList.remove("is-open");
+  bodyScrollLock.enableBodyScroll(document.body);
+});
