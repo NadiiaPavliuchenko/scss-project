ef64bef8286337c1bb3a24a18022df34594b6b97
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-11-05 12:57:55 +0200

    add scroll to section

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
 import "../scss/style.scss";
 import * as bodyScrollLock from "body-scroll-lock";
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 const refs = {
   openMenu: document.querySelector(".burgerBtn"),
   backdrop: document.querySelector(".backdrop"),
+  scroll: document.querySelector(".hero-scroll"),
+  sectionPrices: document.querySelector(".prices"),
 };
 
 function toggleMenu() {
@@ -23,3 +31,12 @@ window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
   refs.backdrop.classList.remove("is-open");
   bodyScrollLock.enableBodyScroll(document.body);
 });
+
+function scrollDown() {
+  gsap.to(window, {
+    duration: 1,
+    scrollTo: refs.sectionPrices,
+  });
+}
+
+refs.scroll.addEventListener("click", scrollDown);
